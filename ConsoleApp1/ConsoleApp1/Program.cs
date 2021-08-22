7bf88ccd7af79d92b727b63eeb6e7974ed732ffa
juanvi159 <juan.obando.a@tecsup.edu.pe>
2021-08-22 15:22:54 -0500

    lab01 - parte 2: suma y raiz

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+        static void Raiz()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("La raiz cuadrada de {0} es {1}", i, Math.Sqrt(i));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Procedimientos y Funciones";
+            string opcion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("[1] SUMA DE DOS NUMEROS");
+                Console.WriteLine("[2] IMPRIMIR LA RAIZ CUADRADA DE LOS PRIMEROS NUMEROS ENTEROS");
+                Console.WriteLine("[0] SALIR");
+                Console.WriteLine("Ingrese una Opcion y Presione Enter");
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese El primer Numero");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese El Segundo Numero");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La suma de {0} y {1} es {2}", a, b, Suma(a,b));
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        Console.WriteLine("Calculando ......");
+                        Raiz();
+                        Console.ReadKey();
+                        break;
+
+                }
+            } while (!opcion.Equals("0"));
+        }
+    }
+}
