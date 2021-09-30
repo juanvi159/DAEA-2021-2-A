d1cc81c24ca9747ffb4960197ac49a1e78817a6c
juanvi159 <juan.obando.a@tecsup.edu.pe>
2021-09-30 10:38:35 -0500

    lab04 - Clonacion de Lab03 y cambio de base de datos

---
+++ Lab04/frmConDB.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab03
+{
+    public partial class frmConDB : Form
+    {
+        SqlConnection conn;
+        public frmConDB()
+        {
+            InitializeComponent();
+        }
+
+        private void frmConDB_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            //Declaramos variables para almacenar los valores de los TextBox
+            // y definimos una cadena
+            String servidor = txtServidor.Text;
+            String bd = txtBaseDatos.Text;
+            String user = txtUsuario.Text;
+            String pwd = txtPassword.Text;
+
+            String str = "Server=" + servidor + ";Database=" + bd + ";";
+
+            //La cadena de conexion cambia en funcion del estado del Checkbox
+            if (chkAutenticacion.Checked)
+                str += "Integrated Security=true";
+            else
+                str += "User Id=" + user + ";Password=" + pwd + ";";
+
+            //Abrir una conexion con el servidor, usando la cadena de conexion
+            try
+            {
+                conn = new SqlConnection(str);
+                conn.Open();
+                MessageBox.Show("Conectado satisfactoriamente");
+                btnDesconectar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar el Servidor: \n" + ex.ToString());
+            }
+        }
+
+        private void btnEstado_Click(object sender, EventArgs e)
+        {
+            //Intentamos obtener el estado de la conexion, y en caso esté abierta,
+            //recuperamos informacion de la misma
+            try
+            {
+                if (conn.State == ConnectionState.Open)
+                    MessageBox.Show("Estado del Servidor: " + conn.State +
+                        "\nVersion del Servidor: " + conn.ServerVersion +
+                        "\nBase de datos: " + conn.Database);
+                else
+                    MessageBox.Show("Estado del Servidor: " + conn.State);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Imposible determinar el estado del Servidor: \n" + ex.ToString());
+            }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            //Para cerrar la conexion verificamos que no este cerrada
+            try
+            {
+                if (conn.State != ConnectionState.Closed)
+                {
+                    conn.Close();
+                    MessageBox.Show("Conexion cerrada satisfactoriamente");
+                    btnDesconectar.Enabled = false;
+                }
+                else
+                    MessageBox.Show("La conexion ya esta cerrada");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ocurrio un error al cerrar la conexion: \n" + ex.ToString());
+            }
+        }
+
+        private void chkAutenticacion_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAutenticacion.Checked)
+            {
+                txtUsuario.Enabled = false;
+                txtPassword.Enabled = false;
+            }
+            else
+            {
+                txtUsuario.Enabled = true;
+                txtPassword.Enabled = true;
+            }
+        }
+
+        private void txtServidor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPersona_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona(conn);
+            persona.Show();
+        }
+    }
+}
