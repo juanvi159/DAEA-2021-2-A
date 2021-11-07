ad15b9da5f2d5678cad7829de6f757ac3ea6b6e6
juanvi159 <juan.obando.a@tecsup.edu.pe>
2021-11-07 08:25:28 -0500

    lab07 - Implementacion del boton buscar

--- lab07/Presentacion/Form1.Designer.cs
+++ lab07/Presentacion/Form1.Designer.cs
@@ -30,6 +30,8 @@ namespace Presentacion
         private void InitializeComponent()
         {
             this.dgDatos = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,23 +43,45 @@ namespace Presentacion
             this.dgDatos.Size = new System.Drawing.Size(776, 359);
             this.dgDatos.TabIndex = 0;
             // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(12, 25);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(554, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(582, 23);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(194, 22);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.dgDatos);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgDatos;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
 
--- lab07/Presentacion/Form1.cs
+++ lab07/Presentacion/Form1.cs
@@ -7,14 +7,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Negocio;
+using System.Data.SqlClient;
 using System.Data;
 
 namespace Presentacion
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
         public Form1()
         {
+            this.conn = conn;
             InitializeComponent();
         }
 
@@ -27,5 +30,36 @@ namespace Presentacion
             dgDatos.DataSource = dt;
             dgDatos.Refresh();
         }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                String FirstName = txtNombre.Text;
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "BuscarPersonaNombre";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Connection = conn;
+
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "FirstName";
+                param.SqlDbType = SqlDbType.NVarChar;
+                param.Value = FirstName;
+
+                cmd.Parameters.Add(param);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dgDatos.DataSource = dt;
+                dgDatos.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("La conexion es cerrada");
+            }
+        }
+
     }
 }
